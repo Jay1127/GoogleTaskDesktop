81d8ba84206f58eca3d3d700e44244c4ad274ce7
Jay1127 <=oksujay1127@gmail.com>
2019-10-07 17:57:11 +0900

    Category 생성

--- GoogleTaskDesktop/ViewModel/CategoryListViewModel.cs
+++ GoogleTaskDesktop/ViewModel/CategoryListViewModel.cs
@@ -1,14 +1,18 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using GoogleTaskDesktop.Core;
+using System;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace GoogleTaskDesktop.ViewModel
 {
     public class CategoryListViewModel : ViewModelBase
     {
-        private bool _isOpen;
+        private bool _isCategoriesShown;
         private CategoryViewModel _currentCategory;
+        private CategoryPopupViewModel _categoryPopupViewModel;
 
         /// <summary>
         /// 카테고리 리스트
@@ -30,24 +34,40 @@ namespace GoogleTaskDesktop.ViewModel
             {
                 Set(ref _currentCategory, value);
 
-                IsOpen = false;
+                IsCategoriesShown = false;
             }
+        }
+
+        /// <summary>
+        /// 카테고리 팝업
+        /// </summary>
+        public CategoryPopupViewModel CategoryPopupViewModel
+        {
+            get => _categoryPopupViewModel;
+            set => Set(ref _categoryPopupViewModel, value);
         }
 
         /// <summary>
         /// 현재 카테고리 리스트가 뷰에 보여지는지 여부
         /// </summary>
-        public bool IsOpen
+        public bool IsCategoriesShown
         {
-            get => _isOpen;
-            set => Set(ref _isOpen, value);
+            get => _isCategoriesShown;
+            set => Set(ref _isCategoriesShown, value);
         }
+
+        /// <summary>
+        /// 새로운 카테고리 생성 팝업 보여주기
+        /// </summary>
+        public RelayCommand ShowNewCategoryCommand { get; }
 
         public CategoryListViewModel(ICategoryList categories)
         {
             Categories = categories;
             CategoryViewModels = new ObservableCollection<CategoryViewModel>(
                 Categories.GetCategories().Select(category => new CategoryViewModel(category)));
+
+            ShowNewCategoryCommand = new RelayCommand(ShowNewCategory);
         }
 
         /// <summary>
@@ -55,7 +75,35 @@ namespace GoogleTaskDesktop.ViewModel
         /// </summary>
         public void ShowPopup()
         {
-            IsOpen = true;
+            IsCategoriesShown = true;            
+        }
+
+        /// <summary>
+        /// 새로운 카테고리 만들기 팝업 호출
+        /// </summary>
+        private void ShowNewCategory()
+        {
+            IsCategoriesShown = false;
+
+            if (CategoryPopupViewModel != null)
+            {
+                CategoryPopupViewModel.CategoryUpdated -= CategoryPopupViewModel_CategoryUpdatedAsync;
+            }
+
+            CategoryPopupViewModel = new CategoryPopupViewModel();
+            CategoryPopupViewModel.CategoryUpdated += CategoryPopupViewModel_CategoryUpdatedAsync;
+
+            CategoryPopupViewModel.ShowPopup();
+        }
+
+        private async Task CategoryPopupViewModel_CategoryUpdatedAsync(string titleToUpdated)
+        {
+            // 처음 생성하는 경우 ID를 부여안함.
+            // ID는 서버에서 자동으로 부여함.(미리 부여하면 에러발생함.)
+            await Categories.AddCategoryAsync(new Category(titleToUpdated));
+            CategoryViewModels.Add(new CategoryViewModel(Categories.GetCategories().Last()));
+
+            CurrentCategory = CategoryViewModels.Last();
         }
     }
 }
---
+++ GoogleTaskDesktop/ViewModel/CategoryPopupViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace GoogleTaskDesktop.ViewModel
+{
+    public class CategoryPopupViewModel : ViewModelBase
+    {
+        private bool _isShown;
+
+        public bool IsShown
+        {
+            get => _isShown;
+            set => Set(ref _isShown, value);
+        }
+
+        public string Title { get; set; }
+
+        public RelayCommand AddCategoryCommand { get; }
+        public RelayCommand CancelCommand { get; }
+
+        public delegate Task CategoryUpdatedHandler(string titleToUpdated);
+
+        public event CategoryUpdatedHandler CategoryUpdated;
+
+        public CategoryPopupViewModel()
+        {
+            AddCategoryCommand = new RelayCommand(AddCategory);
+            CancelCommand = new RelayCommand(Cancel);
+        }
+
+        public void ShowPopup()
+        {
+            IsShown = true;
+        }
+
+        private void AddCategory()
+        {
+            CategoryUpdated?.Invoke(Title);
+            IsShown = false;
+        }
+
+        private void Cancel()
+        {
+            IsShown = false;
+        }
+    }
+}
